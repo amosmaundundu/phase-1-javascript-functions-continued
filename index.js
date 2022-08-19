f2790aa5210d1140bb8f55d72968acb5d43da7d7
amosmaundundu <amosmaundu5@gmail.com>
2022-08-20 00:47:46 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun( activity='rollskate!') {
+ return `This Saturday, I want to ${activity}`;  
+
+
+    }
+function mondayWork(activity='go to the office.'){
+    return `This Monday, I will ${activity}`;
+}
+function wrapAdjective(person='I am'){
+    innerFunction=function(special="*"){
+        return `${person}am a ${special} programmer`;
+
+    }
+ return `${special} programmer`;
+}
+ wrapAdjective();
