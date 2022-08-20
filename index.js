dcb56fe13c4b01b80183ddecaa703de67e02dcfb
amosmaundundu <amosmaundu5@gmail.com>
2022-08-20 15:01:33 +0300

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-function saturdayFun( activity='rollskate!') {
- return `This Saturday, I want to ${activity}`;  
+function saturdayFun(activity='bathe my dog'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+
+function saturdayFun( activity='roller-skate') {
+ return `This Saturday, I want to ${activity}!`;  
 
 
     }
 function mondayWork(activity='go to the office.'){
     return `This Monday, I will ${activity}`;
 }
-function wrapAdjective(person='I am'){
-    innerFunction=function(special="*"){
-        return `${person}am a ${special} programmer`;
+
+
+
+function wrapAdjective(adjective= "*"){
+    function innerFunction(special="programmer"){
+        return `"You are" ${adjective} ${special}`;
 
     }
- return `${special} programmer`;
+
 }
- wrapAdjective();
